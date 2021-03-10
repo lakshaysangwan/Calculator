f8fec03f84e8c96a8cf26a2a90d78a3b22a7710c
Lakshay Sangwan <lakshay.ib.hisar@gmail.com>
2021-03-10 16:04:02 +0530

    Calculator 1.0

--- app.js
+++ app.js
@@ -1,36 +1,73 @@
-const input = document.getElementById('input');
-const body = document.getElementById('main');
+const input = document.getElementById('result');
+
 function clr(){
     input.value="";
 }
-function enter(x){
-    if(x=='left'){
-        input.value+=')';
-    }
-    else if(x=='right'){
-        input.value+='(';
-    }
-    else{
-        input.value+=x;
-    }
-}
 function back(){
-    var hello = input.value.length ;
-    var result='';
-    for(var i=0;i<hello-1;i++){
-        result+=input.value.charAt(i);
+    var value = "";
+    var length = input.value.length;
+    for(var i=0;i<length-1;i++){
+        value+=input.value.charAt(i);
+    }
+    input.value=value;
+}
+function reciprocal(){
+    var value = eval(input.value);
+    input.value="1/"+value;
+}
+function square(){
+    var value = eval(input.value);
+    input.value=value*value;
+}
+function factorial(){
+    var value = eval(input.value);
+    var result = 1;
+    for(var i=1;i<=value;i++){
+        result*=i;
     }
     input.value=result;
 }
-function insert(event){
-    var x = event.key;
-    input.value+=x;
+function randomNo(){
+    input.value=Math.ceil(Math.random()*10);
 }
-input.addEventListener("keypress",function(e){
-    if(e.keyCode == 13){
-        result();
+function calculate(){
+    input.value=eval(input.value);
+}
+function plusMinus(){
+    input.value=eval(input.value);
+    var value = input.value;
+    if(value>0){
+        input.value=value-(value*2);
     }
-    else if(e.keyCode == 27){
+    else{
+        input.value=Math.abs(value);
+    }
+}
+function insert(value){
+    if(value=='left'){
+        input.value+='()';
+    }
+    else if(value=='right'){
+        input.value+=')';
+    }
+    else{
+        input.value+=value;
+    }
+}
+function insertOperation(value){
+    let size = document.getElementById('result').value.length;
+    if(!isNaN(input.value.charAt(size-1))){
+        input.value+=value;
+    }
+    else{
+        input.value.charAt(size-1)=value;
+    }
+}
+function keyPressed(event){
+    if(event.keyCode==13){
+        calculate();
+    }
+    else if(event.keyCode==27){
         clr();
     }
-});
+}
